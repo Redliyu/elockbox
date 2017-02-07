0e220cf571c99e7f37bc7bbd1a81884a0b36ba23
Cheng Zhang <chengzhangusc@hotmail.com>
2017-02-06 17:51:47 -0800

    Modify route

--- routes/web.php
+++ routes/web.php
@@ -76,4 +76,4 @@ Route::get('/logout', ['uses' => 'LoginController@logout']);
 
 //Route::get('/uploadfile', 'FileuploadingController@index');
 //Route::post('/uploadfile', 'FileuploadingController@showfileupload');
-Route::get('/test', ['uses' => 'Admin\CaseManagement\CaseController@viewtest']);
+//Route::get('/test', ['uses' => 'Admin\CaseManagement\CaseController@viewtest']);
