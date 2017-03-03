4975cd6d4f17899e8b6012a32256d76bf08208fa
Cheng Zhang <chengzhangusc@hotmail.com>
2017-03-02 18:45:16 -0800

    day30
    Create activities page. Not finish yet, do not update to server.

--- app/Http/Controllers/Admin/ActivityManagement/ActivityController.php
+++ app/Http/Controllers/Admin/ActivityManagement/ActivityController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\admin\ActivityManagement;
 
 use Illuminate\Http\Request;
-
+use App\User;
+use App\UserRole;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -11,7 +12,25 @@ class ActivityController extends Controller
 {
     //
     public function view() {
-        
-        return view('admin/activity/view');
+        $admins = UserRole::where("role_id", 1)->get();
+        $managers = UserRole::where("role_id", 2)->get();
+        $staffs = UserRole::where("role_id", 3)->get();
+
+        return view('admin/activity/view', [
+            'admins' => $admins,
+            'managers' => $managers,
+            'staffs' => $staffs,
+        ]);
     }
+
+    public function create(Request $request) {
+        echo "create act";
+        echo($request->get('recipient'));
+        echo($request->get('mentioned'));
+        echo($request->get('subject'));
+        echo($request->get('message'));
+
+    }
+
+//    public function
 }
